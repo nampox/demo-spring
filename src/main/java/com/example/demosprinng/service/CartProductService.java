d11764685111caa7ca335de18c9d9573eea857d1
NAMPOX\namdo <namdo26122002@gmail.com>
2023-07-24 23:31:01 +0700

    lan 3 commit

--- src/main/java/com/example/demosprinng/service/CartProductService.java
+++ src/main/java/com/example/demosprinng/service/CartProductService.java
@@ -33,7 +33,7 @@ public class CartProductService {
         try {
             CartProduct cartProduct = cartProductRepository.getById(id);
             cartProductRepository.delete(cartProduct);
-            return "xoa thanh cong oke";
+            return "xoa thanh cong oke lan 2 oke ";
         }catch (Exception exception){
             return "CartProduct ko ton tai";
         }
